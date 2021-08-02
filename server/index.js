842bb03523e3257522be592210098a4da74a3b5c
mitej23 <mitejmadan@gmail.com>
2021-08-02 14:19:12 +0530

    added express socket io server

---
+++ server/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Motto");
+});
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
+
+io.on("connection", (socket) => {
+  // fetch existing users
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  socket.emit("users", users);
+
+  // notify existing users
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.username,
+  });
+
+  // forward the private message to the right recipient
+  socket.on("private message", ({ content, to }) => {
+    socket.to(to).emit("private message", {
+      content,
+      from: socket.id,
+    });
+  });
+
+  // notify users upon disconnection
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
